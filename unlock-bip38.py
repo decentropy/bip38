e59cb27cfe40f841459645fa6eea8b2f16a09cbc
steve-vincent <sav74sac@hotmail.com>
2017-08-14 23:53:59 -0700

    Create unlock-bip38.py

---
+++ unlock-bip38.py
@@ -0,0 +1,24 @@
+from bip38 import *
+from bitcoin import *
+
+
+print "================================================================================="
+print 'BIP38 Unlock: decrypt private key from passphrase encrypted key.'
+print "================================================================================="
+print "(Always ensure security and privacy before exposing private keys)"
+
+print " "
+print 'Enter BIP38 encrypted key:'
+bip = raw_input()
+
+print " "
+print 'Enter secret passphrase:'
+pasw = raw_input()
+print " "
+
+#decrypt
+wif = bip38_decrypt(bip, pasw)
+
+print wif
+print " "
+
