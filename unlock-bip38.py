21d5974870b34f3785d544cb98f2f33c336c3c44
steve-vincent <sav74sac@hotmail.com>
2017-08-15 23:26:41 -0700

    Update unlock-bip38.py

--- unlock-bip38.py
+++ unlock-bip38.py
@@ -2,9 +2,8 @@ from bip38 import *
 from bitcoin import *
 
 
-print "================================================================================="
 print 'BIP38 Unlock: decrypt private key from passphrase encrypted key.'
-print "================================================================================="
+print "====="
 print "(Always ensure security and privacy before exposing private keys)"
 
 print " "
@@ -19,6 +18,7 @@ print " "
 #decrypt
 wif = bip38_decrypt(bip, pasw)
 
-print wif
+print "Address: " + privtoaddr(wif)
+print "Key: " + wif
 print " "
 
